87081556951e8eac9b8cb4dc4424e0a5408840dc
Sampan Verma <himself@samlovescoding.com>
2026-02-05 17:23:21 +0530

    Initial Commit

---
+++ pages/index.blade.php
@@ -0,0 +1,15 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+  //
+};
+?>
+
+<div>
+  <title>Welcome</title>
+  <h1 class="text-4xl">samlovescoding/kit</h1>
+  <flux:text>The ULTRA fastest kit for EVERYTHING - Websites, Desktop Apps and Mobile Apps</flux:text>
+</div>
